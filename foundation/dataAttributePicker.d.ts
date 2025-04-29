74a4060c00cc4ebd80276b6632cfee8538e75a80
Nora Blomaard <n.blomaard@gmail.com>
2025-04-29 10:37:58 +0200

    Updates

---
+++ foundation/dataAttributePicker.d.ts
@@ -0,0 +1,3 @@
+import type { Tree } from '@openscd/oscd-tree-grid';
+export declare function dataAttributeTree(doc: XMLDocument, leafNode?: string): Tree;
+export declare function getSourceDef(paths: string[][]): string[];
